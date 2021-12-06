fa05dcd1f37f73e3acc45cc7513566f3d62eb6ce
naveen0-0 <Naveen>
2021-12-06 18:25:29 +0530

    Done

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ import Settings from '../Settings/Settings'
 export default function HomePage() {
   let components = [<Dashboard/>,<MarketCap/>,<Compare/>,<News/>,<Settings/>]
   
-  const [selectedIndex, setSelectedIndex] = useState(3)
+  const [selectedIndex, setSelectedIndex] = useState(4)
 
   return (
     <div className={styles.main}>
