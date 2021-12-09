150ae1a46f9bad8e3ec9d84dbef2b97a6a29f8e4
naveen0-0 <Naveen>
2021-12-09 06:26:39 +0530

    Made the sidebar to close everytime the user selects a new tab

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -34,7 +34,7 @@ export default function Sidebar({ index, setIndex, setSidebarOpen, sidebarOpen }
           <div>
             <img src={dashboardimg} alt="Dashboard"/>
           </div>
-          <div className={index === 0 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>setIndex(0)}>
+          <div className={index === 0 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>{setSidebarOpen(true);setIndex(0)}}>
             Dashboard
           </div>
         </div>
@@ -44,7 +44,7 @@ export default function Sidebar({ index, setIndex, setSidebarOpen, sidebarOpen }
           <div>
             <img src={barimg} alt="Market Cap"/>
           </div>
-          <div className={index === 1 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>setIndex(1)}>
+          <div className={index === 1 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>{setSidebarOpen(true);setIndex(1)}}>
             MarketCap
           </div>
         </div>
@@ -54,7 +54,7 @@ export default function Sidebar({ index, setIndex, setSidebarOpen, sidebarOpen }
           <div>
             <img src={graphimg} alt="Compare"/>
           </div>
-          <div className={index === 2 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>setIndex(2)}>
+          <div className={index === 2 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>{setSidebarOpen(true);setIndex(2)}}>
             Compare
           </div>
         </div>
@@ -64,7 +64,7 @@ export default function Sidebar({ index, setIndex, setSidebarOpen, sidebarOpen }
           <div>
             <img src={walletimg} alt="News"/>
           </div>
-          <div className={index === 3 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>setIndex(3)}>
+          <div className={index === 3 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>{setSidebarOpen(true);setIndex(3)}}>
             News
           </div>
         </div>
@@ -74,7 +74,7 @@ export default function Sidebar({ index, setIndex, setSidebarOpen, sidebarOpen }
           <div>
             <img src={settingimg} alt="Settings"/>
           </div>
-          <div className={index === 4 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>setIndex(4)}>
+          <div className={index === 4 ? styles.sidebarlinktextactive : styles.sidebarlinktext} onClick={()=>{setSidebarOpen(true);setIndex(4)}}>
             Settings
           </div>
         </div>
